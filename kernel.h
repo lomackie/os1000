10afa6356a2e0fc86a2055c3033e1eb802f8f70d
lomackie <me@louismackie.com>
2025-09-27 21:17:15 +0100

    Add paging

--- kernel.h
+++ kernel.h
@@ -5,10 +5,18 @@
 #define PROCS_UNUSED 0
 #define PROC_RUNNABLE 1
 
+#define SATP_SV32 (1u <<31)
+#define PAGE_V (1 << 0)
+#define PAGE_R (1 << 1)
+#define PAGE_W (1 << 2)
+#define PAGE_X (1 << 3)
+#define PAGE_U (1 << 4)
+
 struct process {
     int pid;
     int state;
     vaddr_t sp;
+    uint32_t *page_table;
     uint8_t stack[8192];
 };
 
@@ -69,3 +77,4 @@ struct trap_frame {
         uint32_t __tmp = (value);                                               \
         __asm__ __volatile__("csrw " #reg ", %0" ::"r"(__tmp));                 \
     } while (0)
+
